260c1fe71368a5a7393d8b155c90171bc0c851cf
Manji Coder <manjicoder@gmail.com>
2024-10-02 19:44:49 +0530

    home page

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 export enum appInfo {
-  title = "ProjectTime",
-  desc = "ProjectTime is a straightforward web app designed for solo users to efficiently track projects and manage their time. Log hours, monitor progress, and visualize tasks easily, helping you stay organized and boost your productivity.",
+  title = 'ProjectTime',
+  desc = 'ProjectTime is a straightforward web app designed for solo users to efficiently track projects and manage their time. Log hours, monitor progress, and visualize tasks easily, helping you stay organized and boost your productivity.',
 }
