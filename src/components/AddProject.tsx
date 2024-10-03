56c05abc40b9bceff63341d04e97ac96a54d1020
Manji Coder <manjicoder@gmail.com>
2024-10-03 15:40:07 +0530

    Add Project hydration error fixed

--- src/components/AddProject.tsx
+++ src/components/AddProject.tsx
@@ -33,67 +33,69 @@ export default function AddProject() {
     setProjects(Array.from(new Set(projects)));
   };
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger>
-        <Button
-          variant={'outline'}
-          className='h-10 w-10 p-0 fixed bottom-24 right-5'
-        >
-          <PlusIcon />
-        </Button>
-      </DialogTrigger>
-      <DialogContent className='max-sm:w-11/12 max-sm:rounded-lg'>
-        <DialogHeader>
-          <DialogTitle hidden>Create project</DialogTitle>
-          <DialogDescription>
-            <Card className='bg-transparent border-none'>
-              <CardHeader>
-                <CardTitle>Create project</CardTitle>
-                <CardDescription>
-                  Create your new project in one-click.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form>
-                  <div className='grid w-full items-center gap-4'>
-                    <div className='flex flex-col space-y-1.5'>
-                      <Label htmlFor='name' className='max-sm:text-left'>
-                        Project Name
-                      </Label>
-                      <Input
-                        id='name'
-                        placeholder='Name of your project'
-                        value={projectName}
-                        onChange={(e) => setProjectName(e.target.value)}
-                      />
+    <>
+      <Button
+        variant={'outline'}
+        className='h-10 w-10 p-0 fixed bottom-24 right-5'
+        onClick={() => setIsOpen(true)}
+      >
+        <PlusIcon />
+      </Button>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger hidden></DialogTrigger>
+        <DialogContent className='max-sm:w-11/12 max-sm:rounded-lg'>
+          <DialogHeader>
+            <DialogTitle hidden>Create project</DialogTitle>
+            <DialogDescription>
+              <Card className='bg-transparent border-none'>
+                <CardHeader>
+                  <CardTitle>Create project</CardTitle>
+                  <CardDescription>
+                    Create your new project in one-click.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <form>
+                    <div className='grid w-full items-center gap-4'>
+                      <div className='flex flex-col space-y-1.5'>
+                        <Label htmlFor='name' className='max-sm:text-left'>
+                          Project Name
+                        </Label>
+                        <Input
+                          id='name'
+                          placeholder='Name of your project'
+                          value={projectName}
+                          onChange={(e) => setProjectName(e.target.value)}
+                        />
+                      </div>
+                      {/* <div className='flex flex-col space-y-1.5'>
+      <Label htmlFor='framework'>Framework</Label>
+      <Select>
+        <SelectTrigger id='framework'>
+          <SelectValue placeholder='Select' />
+        </SelectTrigger>
+        <SelectContent position='popper'>
+          <SelectItem value='next'>Next.js</SelectItem>
+          <SelectItem value='sveltekit'>SvelteKit</SelectItem>
+          <SelectItem value='astro'>Astro</SelectItem>
+          <SelectItem value='nuxt'>Nuxt.js</SelectItem>
+        </SelectContent>
+      </Select>
+    </div> */}
                     </div>
-                    {/* <div className='flex flex-col space-y-1.5'>
-                      <Label htmlFor='framework'>Framework</Label>
-                      <Select>
-                        <SelectTrigger id='framework'>
-                          <SelectValue placeholder='Select' />
-                        </SelectTrigger>
-                        <SelectContent position='popper'>
-                          <SelectItem value='next'>Next.js</SelectItem>
-                          <SelectItem value='sveltekit'>SvelteKit</SelectItem>
-                          <SelectItem value='astro'>Astro</SelectItem>
-                          <SelectItem value='nuxt'>Nuxt.js</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </div> */}
-                  </div>
-                </form>
-              </CardContent>
-              <CardFooter className='flex justify-between'>
-                <Button variant='outline' onClick={closeModal}>
-                  Cancel
-                </Button>
-                <Button onClick={handleSubmit}>Submit</Button>
-              </CardFooter>
-            </Card>
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+                  </form>
+                </CardContent>
+                <CardFooter className='flex justify-between'>
+                  <Button variant='outline' onClick={closeModal}>
+                    Cancel
+                  </Button>
+                  <Button onClick={handleSubmit}>Submit</Button>
+                </CardFooter>
+              </Card>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
